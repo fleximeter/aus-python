330761f0bfe2869e54d55360a2303434c197f22c
Jeff Martin <jeffreymartin@outlook.com>
2023-05-23 20:27:59 -0400

    started work on aiff

--- dev.py
+++ dev.py
@@ -1,7 +1,9 @@
 import numpy as np
 from scipy.io.wavfile import read
+import scipy.fft
 import sampler
 import wav
+import matplotlib.pyplot as plt
 
 file_name = "D:\\Recording\\ReaperProjects\\trombone_samples\\samples\\trombone_sample_mute_9-19-22_a69_p.wav"
 file_name1 = "D:\\Recording\\Samples\\pianobook\\YamahaC7\\YamahaC7\\Samples\\A#5_T1B.wav"
@@ -9,10 +11,64 @@ file_name2 = "D:\\Desktop\\sample24.wav"
 file_name3 = "D:\\Desktop\\sample32.wav"
 file_name4 = "C:\\Users\jeff_martin\Desktop\sample24.wav"
 file_name5 = "C:\\Users\jeff_martin\Desktop\sample32.wav"
+file_name6 = "D:\\Desktop\\sample16.aif"
+file_name7 = "D:\\Desktop\\sample24.aif"
+file_name8 = "D:\\Desktop\\sample32.aif"
+file_name9 = "C:\\Users\jeff_martin\Desktop\sample24.aif"
+file_name10 = "C:\\Users\jeff_martin\Desktop\sample32.aif"
 
-a = wav.read_wav(file_name)
+# a = wav.read_wav(file_name)
 
-points = sampler.detect_loop_points(a, 0, 500)
-print(len(points))
+# x = a.samples[0, 16384:16384+4096]
+# print(x.shape)
 
-wav.visualize_wav(a, 0, points[0])
+# s = scipy.fft.rfft(x)
+# f = scipy.fft.rfftfreq(4096, 1/a.sample_rate)
+
+# print(s.shape)
+# print(f.shape)
+
+# fig, ax = plt.subplots()
+# ax.plot(f, np.abs(s))
+# plt.show()
+
+# http://paulbourke.net/dataformats/audio/
+with open(file_name6, "rb") as audio:
+    HEADER1 = b'FORM'
+    HEADER2 = b'AIFF'
+    HEADER3 = b'COMM'
+    HEADER4 = b'SSND'
+    
+    title = audio.read(4)
+    file_size = audio.read(4)
+    file_size = int.from_bytes(file_size, byteorder="big", signed=False)
+    label1 = audio.read(4)
+    if title != HEADER1 or label1 != HEADER2:
+        pass # error condition 1
+
+    # Read the common chunk
+    label2 = audio.read(4) # HEADER3
+    if label2 != HEADER3:
+        pass # error condition 2
+
+    size = audio.read(4)
+    size = int.from_bytes(size, byteorder="big", signed=False)
+    common_chunk = audio.read(size)
+    
+    # Read the sound chunk
+    label3 = audio.read(4) # HEADER4
+    if label3 != HEADER4:
+        pass # error condition 3
+
+    sound_chunk_size = audio.read(4)
+    sound_chunk_size = int.from_bytes(sound_chunk_size, byteorder="big", signed=False)
+    
+    # Read the offset and block size, which will probably be 0
+    offset = audio.read(4)
+    offset = int.from_bytes(offset, byteorder="big", signed=False)
+    block_size = audio.read(4)
+    block_size = int.from_bytes(block_size, byteorder="big", signed=False)
+    
+    # Read the rest of the file
+    data = audio.read()
+    
--- spectrum.py
+++ spectrum.py
@@ -7,17 +7,36 @@ This file contains functionality for spectral analysis.
 """
 
 import scipy.fft
+import numpy as np
 from wav import AudioFile
 
-def fft(file: AudioFile, channel: int = 0, frames=None):
+def fft(file: AudioFile, channel: int = 0, frames=None, window_size: int = 1024):
     """
     Performs the FFT on an AudioFile.
     :param file: An AudioFile
     :param channel: The channel to analyze
     :param frames: A list or tuple specifying the outer frames of an area to analyze. If None, the entire file will be analyzed.
+    :param window_size: The window size that will be analyzed
     :return: The spectrum of the file
     """
     if frames is None:
-        return scipy.fft.rfft(file.samples[channel, :])
+        x = file.samples[channel, :]
     else:
-        return scipy.fft.rfft(file.samples[channel, frames[0]:frames[1]])
+        x = file.samples[channel, frames[0]:frames[1]]
+    output = None
+    for i in range(0, file.num_frames, window_size):
+        num_samples_in_batch = np.min(file.num_frames - i - 1, window_size)
+        fft_data = scipy.fft.rfft(x[i:i+num_samples_in_batch], n=num_samples_in_batch)
+        fft_data = np.transpose(fft_data)
+        if output is None:
+            output = fft_data
+        else:
+            output = np.hstack(output, fft_data)
+    return output
+
+def plot_fft_data(fft_data):
+    """
+    Plots FFT data
+    :param fft_data: FFT data to plot
+    """
+    pass
