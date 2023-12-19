41da2a959c1ae29133e0e03bd810544dc78da25a
Jeff Martin <jeffreymartin@outlook.com>
2023-12-19 12:27:14 -0600

    added analyzer feature to analyzer module

--- audiopython/analysis.py
+++ audiopython/analysis.py
@@ -7,6 +7,26 @@ Audio analysis tools developed from Eyben, "Real-Time Speech and Music Classific
 """
 
 import numpy as np
+import scipy.fft
+import scipy.signal
+import sklearn.linear_model
+import spectrum
+
+
+def analyzer(audio):
+    """
+    Runs a suite of analysis tools on a provided NumPy array of audio samples
+    :param audio: A 1D NumPy array of audio samples
+    :return: A dictionary with the analysis results
+    """
+    results = {}
+    audio_spectrum = scipy.fft.rfft(audio)
+    magnitude_spectrum, phase_spectrum = spectrum.fft_data_decompose(audio_spectrum)
+    rfftfreqs = scipy.fft.rfftfreq(audio.shape[-1], 1/44100)
+    results['energy'] = energy(audio)
+    results['spectral_slope'] = spectral_slope(magnitude_spectrum, rfftfreqs)
+    results['zero_crossing_rate'] = zero_crossing_rate(audio)
+    return results
 
 
 def energy(audio):
@@ -17,6 +37,17 @@ def energy(audio):
     Reference: Eyben, pp. 21-22
     """
     return np.sqrt((1 / audio.shape[-1]) * np.sum(np.square(audio)))
+
+
+def spectral_slope(magnitude_spectrum, magnitude_freqs):
+    """
+    Calculates the spectral slope from provided magnitude spectrum
+    :param magnitude_spectrum: The magnitude spectrum
+    :param magnitude_freqs: The magnitude frequencies
+    :return: The slope and y-intercept
+    """
+    slope = sklearn.linear_model.LinearRegression().fit(np.reshape(magnitude_spectrum, (magnitude_spectrum.shape[-1], 1)), magnitude_freqs)
+    return slope.coef_[-1], slope.intercept_
 
 
 def zero_crossing_rate(audio, sample_rate):
--- dev.py
+++ dev.py
@@ -2,6 +2,7 @@ import scipy.fft
 import audiopython.sampler as sampler
 import audiopython.spectrum as spectrum
 import audiopython.audiofile as audiofile
+import audiopython.analysis as analysis
 import audio_files
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,25 +11,22 @@ import audiopython.basic_operations as basic_operations
 import pedalboard as pb
 
 
-#f = audiofile.read("D:\\Recording\\Samples\\Iowa\\Cello.arco.mono.1644.1\\ff\\Cello.arco.ff.sulA.A4A5.mono.aif")
-#print(type(f))
-#audiofile.convert(f, "float32")
-#print(f.num_frames)
-#print(np.max(f.samples))
-#print(basic_operations.dbfs(f.samples[:, 243590-5:243590+5]))
-
 audio = None
 
-with pb.io.AudioFile("D:\\Recording\\ReaperProjects\\electronic_music_production\\final_project\\audio\\walk_sign_clinton1.wav", 'r') as infile:
+with pb.io.AudioFile("D:\\Recording\\Samples\\Iowa\\Viola.pizz.mono.1644.1\\Viola.pizz.sulA.mf.A4B4.mono.aif", 'r') as infile:
     sample_rate = infile.samplerate
     frames = infile.frames
     channels = infile.num_channels
-    audio = infile.read(infile.frames)
-    audio_chunk = audio[:, 180000:190000]
-    hpf = pb.HighpassFilter(200)
-    audio_chunk = hpf(audio_chunk, sample_rate)
-    audio_chunk = basic_operations.fade_in(audio_chunk, "hanning", 200)
-    audio_chunk = basic_operations.fade_out(audio_chunk, "hanning", 600)
-    audio_chunk = np.hstack((np.zeros((2, sample_rate)), audio_chunk, np.zeros((2, sample_rate))))
-    with pb.io.AudioFile("D:\\Recording\\ReaperProjects\\electronic_music_production\\final_project\\audio\\walk_sign_word.wav", 'w', sample_rate, channels, 24) as out:
-        out.write(audio_chunk)
+    audio = infile.read(infile.frames)[:, 129186:467424]
+    audio = np.reshape(audio, (audio.shape[-1]))
+
+spec = scipy.fft.rfft(audio)
+mag, phase = spectrum.fft_data_decompose(spec)
+freqs = scipy.fft.rfftfreq(audio.shape[-1], 1/44100)
+
+print(freqs)
+print(mag)
+
+slope, yint = analysis.spectral_slope(mag, freqs)
+print(slope)
+print(yint)
