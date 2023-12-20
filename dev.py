a4becef7d6aec9977abbb3f6059fa61a44cd7b68
Jeff Martin <jeffreymartin@outlook.com>
2023-12-19 18:38:05 -0600

    modified the dev file

--- dev.py
+++ dev.py
@@ -14,13 +14,13 @@ import time
 audio_a4 = None
 audio_c4 = None
 
-with pb.io.AudioFile("D:\\Recording\\Samples\\Iowa\\Viola.pizz.mono.2496\\Viola.pizz.sulG.mf.C4B4.mono.aif", 'r').resampled_to(44100) as infile:
+with pb.io.AudioFile("D:\\Recording\\Samples\\Iowa\\Viola.pizz.mono.2444.1\\Viola.pizz.sulG.mf.C4B4.mono.wav", 'r') as infile:
     sample_rate = infile.samplerate
     frames = infile.frames
     channels = infile.num_channels
     audio = infile.read(infile.frames)
-    audio_c4 = audio[:, 42763:80296]
-    audio_a4 = audio[:, 2789697:2826720]
+    audio_c4 = audio[:, 39282:72576]
+    audio_a4 = audio[:, 2563026:2585448]
     audio_c4 = audio_c4.reshape((audio_c4.size))
     audio_a4 = audio_a4.reshape((audio_a4.size))
 
@@ -32,8 +32,8 @@ audio_analysis = analysis.analyzer(audio_c4, 44100)
 print("C4")
 print(audio_analysis)
 
-print("A4")
 audio_analysis = analysis.analyzer(audio_a4, 44100)
+print("A4")
 print(audio_analysis)
 
 print("Gaussian noise")
