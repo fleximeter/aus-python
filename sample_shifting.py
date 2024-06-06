e70c3ceb89b21b87651c2a7f69447521271bcb74
Jeff Martin <jeffreymartin93@gmail.com>
2024-06-06 17:23:51 -0500

    little tweaks to sample shifting

--- sample_shifting.py
+++ sample_shifting.py
@@ -12,15 +12,15 @@ import scipy.signal as signal
 
 if __name__ == "__main__":
     DIR = "C:\\Users\\jeffr\\Recording\\reaper"
-    FILE = os.path.join(DIR, "photon.wav")
-    OUT = os.path.join(DIR, "photon1.wav")
+    FILE = os.path.join(DIR, "voice1.wav")
+    OUT = os.path.join(DIR, "voice1.1.wav")
     FFT_SIZE = 2048
     audio = audiofile.read(FILE)
     lpf = signal.butter(2, 3000, "low", output="sos", fs=audio.sample_rate)
     # new_samples1 = signal.sosfilt(lpf, exchanger(audio.samples, 8))
     STFT = signal.ShortTimeFFT(signal.windows.hann(FFT_SIZE), FFT_SIZE // 2, audio.sample_rate)
     stft_data = STFT.stft(audio.samples)
-    stft_data = operations.exchanger(stft_data, 8)
+    stft_data = operations.stochastic_exchanger(stft_data, 8)
     out1 = audiofile.AudioFile.copy_header(audio)
     out1.samples = np.array(STFT.istft(stft_data))
     out1.samples = operations.leak_dc_bias_filter(out1.samples)
